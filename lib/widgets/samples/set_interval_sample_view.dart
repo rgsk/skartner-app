60394cf6874dd2c0d50eb1f13df0f2f880b55000
rgsk <guptarahul70322@gmail.com>
2023-11-06 06:35:15 +0530

    eg

--- lib/widgets/samples/set_interval_sample_view.dart
+++ lib/widgets/samples/set_interval_sample_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:skartner_app/hooks/general/use_set_interval.dart';
+import 'package:skartner_app/providers/counter_provider.dart';
 
 class SetIntervalSampleView extends HookConsumerWidget {
   const SetIntervalSampleView({super.key});
@@ -18,7 +19,9 @@ class SetIntervalSampleView extends HookConsumerWidget {
             },
             child: Text('Toggle'),
           ),
-          if (show.value) SetIntervalSampleViewChild()
+          // if (show.value) SetIntervalSampleViewChild()
+
+          ContinuedStateProviderChange(),
         ],
       ),
     );
@@ -84,6 +87,44 @@ class ContinuedStateChange extends HookConsumerWidget {
           ElevatedButton(
             onPressed: () {
               counter.value++;
+            },
+            child: Text('increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContinuedStateProviderChange extends HookConsumerWidget {
+  const ContinuedStateProviderChange({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final counter = ref.watch(counterProvider);
+    final intervalHandler = useSetInterval();
+
+    useEffect(() {
+      final every1Second = intervalHandler.setInterval(() {
+        print('every 1 second');
+        ref.watch(counterProvider.notifier).state++;
+      }, 1000);
+      final every2Second = intervalHandler.setInterval(() {
+        print('every 2 second');
+        print(counter);
+        print(
+            ref.watch(counterProvider)); // this would give us the latest value
+      }, 1000);
+
+      return null;
+    }, []);
+    return Container(
+      child: Column(
+        children: [
+          Text('Counter: ${counter}'),
+          ElevatedButton(
+            onPressed: () {
+              ref.watch(counterProvider.notifier).state += 1;
             },
             child: Text('increment'),
           ),
