2e958f2cd716e59402522b931ccb9f2656b01c23
rgsk <guptarahul70322@gmail.com>
2023-12-20 15:59:57 +0530

    deploy script

--- bin/web/deploy.sh
+++ bin/web/deploy.sh
@@ -1,10 +1,16 @@
-cp .env ../envs/skartner-app/temp.env
-cp ../envs/skartner-app/staging.env .env
+cp .env ../skartner-instance/envs/skartner-app/temp.env
+cp ../skartner-instance/envs/skartner-app/staging.env .env
 
 flutter build web
 
-cp ../envs/skartner-app/temp.env .env
+cp ../skartner-instance/envs/skartner-app/temp.env .env
 
 docker build . --platform linux/amd64 -t rgskartner/skartner-app
 
 docker push rgskartner/skartner-app
+
+source ../skartner-instance/envs/skartner-jenkins/staging.env
+
+# echo "JENKINS_API_KEY: $JENKINS_API_KEY"
+
+curl -X POST http://jenkins.skartner.com/job/restart-skartner-app/build --user dev-rahul:$JENKINS_API_KEY
